127716bf60148429b4118a79f1cdd456dbbe1af1
VirtualVasquez <mxv647@med.miami.edu>
2021-01-14 17:44:32 -0500

    edited proxy and server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import {ApolloProvider } from '@apollo/react-hooks'
 import {setContext} from 'apollo-link-context'
 
 const httpLink = createHttpLink({
-    uri: ' https://calm-eyrie-20998.herokuapp.com/'
+    uri: 'https://protected-sea-28639.herokuapp.com/'
 })
 
 const authLink = setContext(() =>{
